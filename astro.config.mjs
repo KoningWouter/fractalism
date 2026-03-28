5586699d25a9295f95a3f8b54c1c57c4db155b5a
Wouter Rengelink <wouterr@gmail.com>
2026-03-28 04:47:00 +0100

    Added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fractalisme.nl',
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [sitemap()]
 });
