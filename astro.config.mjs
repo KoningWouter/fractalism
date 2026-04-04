4cd8e927a17c27957f4fb04782189ecae65c1133
Wouter Rengelink <wouterr@gmail.com>
2026-04-04 22:42:53 +0200

    Added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
+import vercel from '@astrojs/vercel';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fractalisme.nl',
-  adapter: cloudflare(),
+  adapter: vercel(),
   integrations: [sitemap()]
 });
